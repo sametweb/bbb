ddc2f8e06aeb34fe9970d3d2ccd2303bfd4234e1
sametweb <sametmutevellioglu@gmail.com>
2020-12-09 05:23:49 -0800

    port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const server = express();
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
 
