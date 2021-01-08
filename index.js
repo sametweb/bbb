7cfd28fcb6a122661ee09c343c27f51b17f45b5c
sametweb <sametmutevellioglu@gmail.com>
2021-01-08 06:23:28 -0800

    gmail test

--- index.js
+++ index.js
@@ -53,10 +53,16 @@ server.get("/", (req, res) => {
 
 server.post("/contact", (req, res) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.GMAIL_USER,
       pass: process.env.GMAIL_PASS,
+    },
+    tls: {
+      // do not fail on invalid certs
+      rejectUnauthorized: false,
     },
   });
 
